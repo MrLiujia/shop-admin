623c76c1b33613e28d1c9217e366cb533a71a163
Mr Liu <1056033459@qq.com>
2018-07-16 20:15:45 +0800

    配置MyBatis

--- src/main/java/shopadmin/config/AppConfig.java
+++ src/main/java/shopadmin/config/AppConfig.java
@@ -1,7 +1,14 @@
 package shopadmin.config;
 
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,4 +21,23 @@ public class AppConfig extends WebMvcConfigurerAdapter{
 	 public void configureViewResolvers(ViewResolverRegistry registry) {
 	        registry.jsp("/WEB-INF/jsp/", ".jsp");
 	    }
+	 
+	    @Bean
+	    public DataSource dataSource(Environment env) { 
+	        DriverManagerDataSource ds = new DriverManagerDataSource(
+	                env.getProperty("jdbc.url"),
+	                env.getProperty("jdbc.username"),
+	                env.getProperty("jdbc.password"));
+	        ds.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+	        return ds;
+	    }   
+	    
+	    @Bean 
+	    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+	        SqlSessionFactoryBean sf = new SqlSessionFactoryBean();
+	        sf.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+	        sf.setDataSource(dataSource);
+	        return sf;
+	    }
+
 }
