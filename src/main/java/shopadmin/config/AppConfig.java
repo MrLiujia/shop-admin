a2dad23277eec187a58ef1262c336a8d6489f1f9
Mr Liu <1056033459@qq.com>
2018-07-16 20:25:25 +0800

    配置Spring Security

--- src/main/java/shopadmin/config/AppConfig.java
+++ src/main/java/shopadmin/config/AppConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -38,6 +40,11 @@ public class AppConfig extends WebMvcConfigurerAdapter{
 	        sf.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
 	        sf.setDataSource(dataSource);
 	        return sf;
+	    }
+	    
+	    @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
 	    }
 
 }
